b0882536e1373fba6a36d111f8e215568ac3b32b
Fabio Petrone <fa610movic90@gmail.com>
2021-01-07 17:25:30 +0100

    Inserita traccia

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,19 +1,7 @@
-// var parola=prompt('Inserisci una parola');
-// var parolarovescia=[];
-// for (var i=parola.length-1; i>=0;i--) {
-// parolarovescia.push(parola[i]);
-// }
-// parolarovescia=parolarovescia.join('');
-//
-// if(parola==parolarovescia)
-// {
-//   alert('la parola è palindroma')
-// }
-// else{
-//   alert('la parola non è palindroma')
-// }
+// Palindroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
 
-// Sopra è la base di partenza
 var input=prompt('Inserisci una parola');
 
 function isPalindroma(parola){
@@ -38,3 +26,7 @@ else{
 }
 
 isPalindroma(input);
+
+// Nota:Al solito parolarovescia anziché essere trasformato in array e poi riempito col push poteva rimanere stringa così come era,inizializzata con ''
+// e poi nel for parolarovescia+=parola[i] evitando join
+// Nota 2:Versione bonus con function reverse
