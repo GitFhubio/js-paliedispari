41484258bf02041589c1d189cd110ae64fdb2c97
Fabio Petrone <fa610movic90@gmail.com>
2021-01-07 22:22:29 +0100

    readme

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -2,8 +2,8 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
-var parolaUtente='';
-
+var parolaUtente;
+var parolarovescia;
 function isValida(parola){
   if(isNaN(parola) && parola.length>2){
   return true;
@@ -12,12 +12,12 @@ else{
   return false;
 }
 }
-
+parolaUtente='';
 while(!isValida(parolaUtente))
 {parolaUtente=prompt('Inserisci una parola');}
 
 function isPalindroma(parola){
-var parolarovescia=[];
+parolarovescia=[];
 // Nota1:Al solito parolarovescia anziché essere array poi riempito col push poteva rimanere stringa inizializzata con ''
 // e poi nel for parolarovescia+=parola[i] evitando join
 parola=parola.toLowerCase();
